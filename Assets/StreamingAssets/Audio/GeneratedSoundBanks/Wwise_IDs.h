405e566babceffef7b3e0789bdc143dd37cdd872
Mr-milky-way <admilkyway@gmail.com>
2026-08-13 17:16:35 -0700

    All files Steam Verion try 20

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,348 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PAUSE_GAME_MUSIC = 2414542046U;
+        static const AkUniqueID PAUSE_GAME_SOUNDS_ALL = 1063484697U;
+        static const AkUniqueID PLAY_DRONE_CRASH = 1314565936U;
+        static const AkUniqueID PLAY_DRONE_FLIPPED = 3318944185U;
+        static const AkUniqueID PLAY_DRONE_MOTORS = 2842915193U;
+        static const AkUniqueID PLAY_DRONE_MOTORS_IDLE = 3988346066U;
+        static const AkUniqueID PLAY_DRONE_MOTORS_START = 3276540896U;
+        static const AkUniqueID PLAY_DRONE_PART_HIT = 2740058106U;
+        static const AkUniqueID PLAY_DRONE_PROP_HIT = 3763294454U;
+        static const AkUniqueID PLAY_DRONE_RESPAWN = 4027370605U;
+        static const AkUniqueID PLAY_DRONE_SPAWN = 1629787408U;
+        static const AkUniqueID PLAY_MX_GARAGE = 898303491U;
+        static const AkUniqueID PLAY_MX_INGAME = 905593515U;
+        static const AkUniqueID PLAY_MX_MAINMENU = 1924038202U;
+        static const AkUniqueID PLAY_MX_MAPEDITOR = 2799829073U;
+        static const AkUniqueID PLAY_MX_POSTGAME = 253301956U;
+        static const AkUniqueID PLAY_MX_RACE = 2272270883U;
+        static const AkUniqueID PLAY_MX_REPLAY = 2262774601U;
+        static const AkUniqueID PLAY_SX_AMB__LV_BELL_LABS = 4140191953U;
+        static const AkUniqueID PLAY_SX_AMB__LV_GATES_OF_HELL = 2248490305U;
+        static const AkUniqueID PLAY_SX_AMB__LV_MIAMI_LIGHTS = 688166750U;
+        static const AkUniqueID PLAY_SX_ENV_WATER = 3367909399U;
+        static const AkUniqueID PLAY_SX_ENV_WIND = 1858511864U;
+        static const AkUniqueID PLAY_SX_GAME_BALLOON_POP = 3932501856U;
+        static const AkUniqueID PLAY_SX_GAME_BALLOON_POP2D = 10969138U;
+        static const AkUniqueID PLAY_SX_GAME_BALLOON_RADAR = 976875145U;
+        static const AkUniqueID PLAY_SX_GAME_BIG_STEP_COMPLETE = 145625842U;
+        static const AkUniqueID PLAY_SX_GAME_COUNTDOWN_GO = 2712861725U;
+        static const AkUniqueID PLAY_SX_GAME_COUNTDOWN_TICK = 2096664328U;
+        static const AkUniqueID PLAY_SX_GAME_FAILURE = 3434682103U;
+        static const AkUniqueID PLAY_SX_GAME_GATE_CLEAR = 2950130732U;
+        static const AkUniqueID PLAY_SX_GAME_GATE_CLEAR_FINAL = 2547790753U;
+        static const AkUniqueID PLAY_SX_GAME_LAP_COMPLETE = 1608524552U;
+        static const AkUniqueID PLAY_SX_GAME_RADAR = 3023850269U;
+        static const AkUniqueID PLAY_SX_GAME_SIGNAL = 434814927U;
+        static const AkUniqueID PLAY_SX_GAME_SMALL_STEP_COMPLETE = 438921789U;
+        static const AkUniqueID RESUME_GAME_MUSIC = 3253464573U;
+        static const AkUniqueID RESUME_GAME_SOUNDS_ALL = 297367300U;
+        static const AkUniqueID STOP_DRONE_MOTORS = 426288351U;
+        static const AkUniqueID STOP_DRONE_SOUNDS_ALL = 1930185201U;
+        static const AkUniqueID STOP_GAME_SOUNDS_ALL = 2907820913U;
+        static const AkUniqueID STOP_MX_ALL = 2202668015U;
+        static const AkUniqueID STOP_MX_GARAGE = 932551597U;
+        static const AkUniqueID STOP_MX_INGAME = 1309319929U;
+        static const AkUniqueID STOP_MX_MAINMENU = 4272494312U;
+        static const AkUniqueID STOP_MX_MAPEDITOR = 3830401755U;
+        static const AkUniqueID STOP_MX_POSTGAME = 1772319926U;
+        static const AkUniqueID STOP_MX_RACE = 761739809U;
+        static const AkUniqueID STOP_MX_REPLAY = 2202970819U;
+        static const AkUniqueID STOP_SX_AMB__LV_BELL_LABS = 674920339U;
+        static const AkUniqueID STOP_SX_AMB__LV_GATES_OF_HELL = 408627291U;
+        static const AkUniqueID STOP_SX_AMB__LV_MIAMI_LIGHTS = 1732058328U;
+        static const AkUniqueID STOP_SX_ENV_WATER = 1875828441U;
+        static const AkUniqueID STOP_SX_ENV_WIND = 3064839254U;
+        static const AkUniqueID STOP_SX_GAME_BALLOON_RADAR = 1031595655U;
+        static const AkUniqueID STOP_SX_GAME_RADAR = 690784207U;
+        static const AkUniqueID STOP_SX_GAME_SIGNAL = 2835567529U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace DRONETYPE
+        {
+            static const AkUniqueID GROUP = 2810826905U;
+
+            namespace STATE
+            {
+                static const AkUniqueID EXTRA = 3311862193U;
+                static const AkUniqueID PLAYER = 1069431850U;
+            } // namespace STATE
+        } // namespace DRONETYPE
+
+        namespace GAMESTATUS
+        {
+            static const AkUniqueID GROUP = 1045871717U;
+
+            namespace STATE
+            {
+                static const AkUniqueID PAUSED = 319258907U;
+                static const AkUniqueID PLAYING = 1852808225U;
+            } // namespace STATE
+        } // namespace GAMESTATUS
+
+        namespace GAMETYPE
+        {
+            static const AkUniqueID GROUP = 1845460495U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAMPAIGN = 30729851U;
+                static const AkUniqueID FREESTYLE = 1242303306U;
+                static const AkUniqueID MISSION_GENERIC = 3137067327U;
+                static const AkUniqueID MISSION_LOS = 2206546786U;
+                static const AkUniqueID RACE = 1875627670U;
+            } // namespace STATE
+        } // namespace GAMETYPE
+
+        namespace RACELEVEL
+        {
+            static const AkUniqueID GROUP = 2442081200U;
+
+            namespace STATE
+            {
+                static const AkUniqueID AIRPLANE_GRAVEYARD = 2242113225U;
+                static const AkUniqueID ATLANTA_AFTERMATH = 674960167U;
+                static const AkUniqueID BELL_LABS = 2324910105U;
+                static const AkUniqueID BOSTON_FOUNDRY = 3870214768U;
+                static const AkUniqueID DETROIT = 2118182778U;
+                static const AkUniqueID DRL_SANDBOX = 1234597685U;
+                static const AkUniqueID FIELD = 514358619U;
+                static const AkUniqueID GATES_OF_HELL = 2503490217U;
+                static const AkUniqueID HOUSE = 3720479411U;
+                static const AkUniqueID LAPOCALYPSE = 2202639834U;
+                static const AkUniqueID MARDI_GRAS = 3570271506U;
+                static const AkUniqueID MIAMI_LIGHTS = 908806678U;
+                static const AkUniqueID MIAMI_NIGHTS = 3855206896U;
+                static const AkUniqueID OHIO_CRASH_SITE = 1486201574U;
+            } // namespace STATE
+        } // namespace RACELEVEL
+
+        namespace RACERESULT
+        {
+            static const AkUniqueID GROUP = 1980469977U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CRASHED = 4265657233U;
+                static const AkUniqueID FINISHED = 3683254051U;
+                static const AkUniqueID NEW_RECORD = 1861161479U;
+                static const AkUniqueID TIMEOUT = 4043259814U;
+            } // namespace STATE
+        } // namespace RACERESULT
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace PROPTYPE_BLADES
+        {
+            static const AkUniqueID GROUP = 3003011824U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BLADE_2B = 3961445304U;
+                static const AkUniqueID BLADE_3B = 3978222863U;
+            } // namespace SWITCH
+        } // namespace PROPTYPE_BLADES
+
+        namespace PROPTYPE_SIZE
+        {
+            static const AkUniqueID GROUP = 3733082794U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SIZE_30 = 801300888U;
+                static const AkUniqueID SIZE_35 = 801300893U;
+                static const AkUniqueID SIZE_40 = 918744221U;
+                static const AkUniqueID SIZE_45 = 918744216U;
+                static const AkUniqueID SIZE_50 = 901966634U;
+                static const AkUniqueID SIZE_55 = 901966639U;
+                static const AkUniqueID SIZE_60 = 885189047U;
+            } // namespace SWITCH
+        } // namespace PROPTYPE_SIZE
+
+        namespace PROPTYPE_TYPE
+        {
+            static const AkUniqueID GROUP = 1572718981U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID TYPE_BC = 2177566237U;
+                static const AkUniqueID TYPE_BN = 2177566224U;
+                static const AkUniqueID TYPE_HN = 2009790126U;
+                static const AkUniqueID TYPE_PN = 2412453094U;
+                static const AkUniqueID TYPE_S0 = 2462785905U;
+            } // namespace SWITCH
+        } // namespace PROPTYPE_TYPE
+
+        namespace RACEMUSIC2SECTIONS_36PERC
+        {
+            static const AkUniqueID GROUP = 959619413U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID INTRO = 1125500713U;
+                static const AkUniqueID SECTION1 = 3524678539U;
+                static const AkUniqueID SECTION2 = 3524678536U;
+            } // namespace SWITCH
+        } // namespace RACEMUSIC2SECTIONS_36PERC
+
+        namespace RACEMUSIC2SECTIONS_42PERC
+        {
+            static const AkUniqueID GROUP = 2261689580U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID INTRO = 1125500713U;
+                static const AkUniqueID SECTION1 = 3524678539U;
+                static const AkUniqueID SECTION2 = 3524678536U;
+            } // namespace SWITCH
+        } // namespace RACEMUSIC2SECTIONS_42PERC
+
+        namespace RACEMUSICPARTSELECTOR
+        {
+            static const AkUniqueID GROUP = 2463383119U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PART1 = 3177314147U;
+                static const AkUniqueID PART2 = 3177314144U;
+                static const AkUniqueID RANDOMPICK = 444437171U;
+            } // namespace SWITCH
+        } // namespace RACEMUSICPARTSELECTOR
+
+        namespace RACEMUSICPARTSELECTOR2
+        {
+            static const AkUniqueID GROUP = 1931351663U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID PART1 = 3177314147U;
+                static const AkUniqueID PART2 = 3177314144U;
+                static const AkUniqueID RANDOMPICK = 444437171U;
+            } // namespace SWITCH
+        } // namespace RACEMUSICPARTSELECTOR2
+
+        namespace ROTSPEEDIN
+        {
+            static const AkUniqueID GROUP = 1057259052U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ENGAGE = 418248564U;
+            } // namespace SWITCH
+        } // namespace ROTSPEEDIN
+
+        namespace ROTSPEEDINCREASE
+        {
+            static const AkUniqueID GROUP = 3685888757U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ENGAGE = 418248564U;
+            } // namespace SWITCH
+        } // namespace ROTSPEEDINCREASE
+
+        namespace ROTSPEEDOUT
+        {
+            static const AkUniqueID GROUP = 1728290763U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ENGAGE = 418248564U;
+            } // namespace SWITCH
+        } // namespace ROTSPEEDOUT
+
+        namespace ROTSPEEDTYPE
+        {
+            static const AkUniqueID GROUP = 3399078365U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID IN = 1752637612U;
+                static const AkUniqueID OUT = 645492555U;
+            } // namespace SWITCH
+        } // namespace ROTSPEEDTYPE
+
+        namespace RPMCHANGES
+        {
+            static const AkUniqueID GROUP = 4008492227U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BIG_DROP = 3276490039U;
+                static const AkUniqueID SMALL_DROP = 3530746366U;
+            } // namespace SWITCH
+        } // namespace RPMCHANGES
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DRONE_EXTRA_SFX_VOLUME = 700964219U;
+        static const AkUniqueID DRONE_IMPACT = 1671140450U;
+        static const AkUniqueID DRONE_ROTSPEED = 963763164U;
+        static const AkUniqueID DRONE_ROTSPEED_DELTA = 3549367207U;
+        static const AkUniqueID DRONE_ROTSPEED_DIR = 3731592192U;
+        static const AkUniqueID DRONE_SPEED = 1382379429U;
+        static const AkUniqueID GATES_PERCENTAGE = 3469933102U;
+        static const AkUniqueID MOTORS_AVERAGE_RPM = 4115984233U;
+        static const AkUniqueID MOTORS_AVERAGE_RPM_DELTA = 3601095406U;
+        static const AkUniqueID MOTORS_MAX_RPM = 2759359202U;
+        static const AkUniqueID RADAR_PROXIMITY = 389286125U;
+        static const AkUniqueID RADIO_SIGNAL = 2419729085U;
+        static const AkUniqueID TIMESCALE = 4070528516U;
+        static const AkUniqueID VOLUME_MX = 319736021U;
+        static const AkUniqueID VOLUME_SX = 4111374603U;
+        static const AkUniqueID VOLUME_VX = 4161707604U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC_INGAME = 2186578306U;
+        static const AkUniqueID MUSIC_MAIN = 1615767906U;
+        static const AkUniqueID MUSIC_POSTGAME = 326064081U;
+        static const AkUniqueID MUSIC_RACE = 282176238U;
+        static const AkUniqueID SFXS = 602065129U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID DX = 1836525785U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MX = 1685527054U;
+        static const AkUniqueID SX = 1584861564U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID AMBIENCE_REVERB = 1690089400U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
